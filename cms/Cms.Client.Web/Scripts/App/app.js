b83ff924bf5887f6b4baf5d95cb3359ac11e3aef
unknown <iboateng051@DSK-ENG-01808.sig.ads>
2014-09-09 17:38:42 +0100

    Added cms client web initial controllers and template files.

---
+++ cms/Cms.Client.Web/Scripts/App/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+
+var app = angular.module('app', ['ngRoute', 'ngSanitize']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/:id/home', {
+        templateUrl: 'Templates/Home.html',
+        controller: 'homeController'
+    });
+    
+    $routeProvider.when('/search', {
+        templateUrl: 'Templates/Search.html',
+        controller: 'searchController'
+    });
+
+    $routeProvider.otherwise({ redirectTo: '/search' });
+});
+
+app.run(function($rootScope) {
+    console.log($rootScope);
+});
